2bf258d5d2945631a641b7b43404a9fcced88a39
Martinez Garrido Angel Ivan <ben6085@gmail.com>
2021-06-24 14:11:08 -0500

    estilos y logica del header terminadas

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,6 +4,34 @@ import Header from '@templates/header.html';
 const loadHeader=()=>{
     const header=document.querySelector('.header');
     header.innerHTML=Header;
+    menuLogic();
+}
+
+const menuLogic=()=>{
+    const menuIconClose=document.querySelector('.menu__icon-close');
+    const menuItems=document.querySelectorAll('.menu__link');
+    const menuIconOpen=document.querySelector('.header__icon-open');
+
+    clickOneElement(menuIconOpen);
+    clickOneElement(menuIconClose);
+    clickElements(menuItems);
+    closeWithBody();
+}
+
+const clickOneElement=(element)=>{
+    const menu=document.querySelector('.menu');
+    element.addEventListener('click',()=>{
+        menu.classList.toggle('open-menu');
+    });
+}
+
+const clickElements=(links)=>{
+    const menu=document.querySelector('.menu');
+    links.forEach(link => {
+        link.addEventListener('click',()=>{
+            menu.classList.toggle('open-menu');
+        });
+    });
 }
 
 export default loadHeader;
